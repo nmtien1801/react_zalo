7a424660aeef0bbd96b799acf4bd49a592dc87ab
nguyenluc25 <nguyenluc1234012@gmail.com>
2025-03-02 23:31:21 +0700

    giao dien thong tin tai khoan 90%, giao dien thong tin nhom 90%, 2 giao dien con lai chuan bi lam

---
+++ src/page/index.js
@@ -0,0 +1,13 @@
+import AccountInfo from "./accountInfo/accountInfo";
+import GroupInfo from "./groupInfo/groupInfo";
+
+function Index() {
+    return (
+        <div>
+            <AccountInfo />
+            <GroupInfo />
+        </div>
+    );
+}
+
+export default Index;

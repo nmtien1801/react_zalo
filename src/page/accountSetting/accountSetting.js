e22e9c1470a67fdf0002261ef13f58b2bed7dac3
khangdora <khangdora2809@gmail.com>
2025-03-03 01:57:17 +0700

    update setting model

---
+++ src/page/accountSetting/accountSetting.js
@@ -0,0 +1,137 @@
+import React, { useEffect, useState } from "react";
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import { Dropdown } from 'react-bootstrap';
+
+import './accountSetting.css';
+
+const CustomModal = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+    const [isDataSubmenuOpen, setIsDataSubmenuOpen] = useState(false);
+    const [isLanguageSubmenuOpen, setIsLanguageSubmenuOpen] = useState(false);
+    const [isChatHelpSubmenuOpen, setIsChatHelpSubmenuOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        if(!isOpen) {
+            setIsDataSubmenuOpen(false);
+            setIsLanguageSubmenuOpen(false);
+            setIsChatHelpSubmenuOpen(false);
+        }
+        setIsOpen(!isOpen);
+    };
+
+    const toggleDataSubmenu = () => {
+        setIsDataSubmenuOpen(!isDataSubmenuOpen);
+        setIsLanguageSubmenuOpen(false);
+        setIsChatHelpSubmenuOpen(false);
+    };
+    
+    const toggleLanguageSubmenu = () => {
+        setIsLanguageSubmenuOpen(!isLanguageSubmenuOpen);
+        setIsDataSubmenuOpen(false);
+        setIsChatHelpSubmenuOpen(false);
+    };
+
+    const toggleChatHelpSubmenu = () => {
+        setIsChatHelpSubmenuOpen(!isChatHelpSubmenuOpen);
+        setIsDataSubmenuOpen(false);
+        setIsLanguageSubmenuOpen(false);
+    };
+    
+    const handleItemClick = (content) => {
+        setIsOpen(false); 
+    };
+
+    return (
+        <div className="custom-dropdown">
+          <button className="leftbar-tab" onClick={toggleDropdown}>
+            <div class="mmi-icon-wr">
+                <div class="z-noti-badge-container">
+                    <i class="fa fa-cog internal-icon"></i>
+                </div>
+            </div>
+          </button>
+          {isOpen && (
+            <div className="zmenu-body has-submenu">
+                <div className="zmenu-item" onClick={() => handleItemClick('Thông tin tài khoản')}>
+                    <i className="fa fa-user menu-icon left"></i>
+                    <span>Thông tin tài khoản</span>
+                </div>
+                <div className="zmenu-item" onClick={() => handleItemClick('Cài đặt')}>
+                    <i className="fa fa-cog menu-icon left"></i>
+                    <span>Cài đặt</span>
+                </div>
+                <div className="zmenu-separator"></div>
+                <div className="zmenu-item has-submenu" onClick={toggleDataSubmenu}>
+                    <i className="fa fa-database menu-icon left"></i>
+                    <span>Dữ liệu</span>
+                    <i className="fa fa-angle-right trailing-icon"></i>
+                </div>
+                {isDataSubmenuOpen && (
+                    <div className="zmenu-sub">
+                    <div className="zmenu-item" onClick={() => handleItemClick('Đồng bộ tin nhắn')}>
+                        Đồng bộ tin nhắn
+                    </div>
+                    <div className="zmenu-item" onClick={() => handleItemClick('Quản lý dữ liệu')}>
+                        Quản lý dữ liệu
+                    </div>
+                    <div className="zmenu-separator"></div>
+                    <div className="zmenu-item" onClick={() => handleItemClick('Khác')}>
+                        Khác
+                    </div>
+                    </div>
+                )}
+                <div className="zmenu-item has-submenu" onClick={toggleLanguageSubmenu}>
+                    <i className="fa fa-language menu-icon left"></i>
+                    <span>Ngôn ngữ</span>
+                    <i className="fa fa-angle-right trailing-icon"></i>
+                </div>
+                {isLanguageSubmenuOpen && (
+                    <div className="zmenu-sub">
+                        <div className="zmenu-item" onClick={() => handleItemClick('Tiếng Việt')}>
+                            Tiếng Việt
+                        </div>
+                        <div className="zmenu-item" onClick={() => handleItemClick('English')}>
+                            English
+                        </div>
+                    </div>
+                )}
+                <div className="zmenu-item has-submenu" onClick={toggleChatHelpSubmenu}>
+                    <i className="fa fa-question-circle menu-icon left"></i>
+                    <span>Hỗ trợ</span>
+                    <i className="fa fa-angle-right trailing-icon"></i>
+                </div>
+                {isChatHelpSubmenuOpen && (
+                    <div className="zmenu-sub">
+                        <div className="zmenu-item" onClick={() => handleItemClick('Tiếng Việt')}>
+                            Thông tin phiên bản
+                        </div>
+                        <div className="zmenu-item" onClick={() => handleItemClick('English')}>
+                            Liên hệ
+                        </div>
+                        <div className="zmenu-item" onClick={() => handleItemClick('English')}>
+                            Gửi file log đến Zalo
+                        </div>
+                        <div className="zmenu-separator"></div>
+                        <div className="zmenu-item" onClick={() => handleItemClick('English')}>
+                           Phím tắt
+                        </div>
+                    </div>
+                )}
+                <div className="zmenu-separator"></div>
+                <div className="zmenu-item" onClick={() => handleItemClick('Đăng xuất')}>
+                    <i className="menu-icon left"></i>
+                    <span className="logout">Đăng xuất</span>
+                </div>
+                <div className="zmenu-item" onClick={() => handleItemClick('Đăng xuất')}>
+                    <i className="menu-icon left"></i>
+                    <span>Thoát</span>
+                </div>
+            </div>
+        )}
+        </div>
+      );
+};
+
+export default CustomModal;
--- src/route/Header.js
+++ src/route/Header.js
@@ -5,30 +5,59 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
+
+import AccountSetting from "../page/accountSetting/accountSetting";
+
+import "./Header.css";
+
 const Header = () => {
+
+  const [showModal, setShowModal] = useState(false);
+  const [modalContent, setModalContent] = useState('');
+
   return (
-    <Navbar
-      className="bg-body-tertiary shadow-sm d-flex flex-column vh-100 border border-dark w-25"
-      sticky="top"
-    >
-      <Container fluid className="d-flex flex-column align-items-start">
-        {/* Logo hoặc tên ứng dụng */}
-        <Navbar.Brand as={NavLink} to="/login" className="fw-bold">
-          Zalo
-        </Navbar.Brand>
+    <Navbar className="main-tab"sticky="top" >
+
+      <Container fluid className="nav__tabs__top">
+
+        <div class="nav__tabs__zalo web" title="Võ Trường Khang">
+          <div class="rel zavatar-container clickable" idelement="avatar">
+            <div class="zavatar zavatar-l zavatar-single flx flx-al-c flx-center rel disableDrag clickable">
+              <img src="https://s120-ava-talk.zadn.vn/2/2/d/9/20/120/0e029b40ab888036e163cd19734fe529.jpg" class="a-child" />
+            </div>
+          </div>
+        </div>
 
         {/* Phần nội dung menu */}
         <Nav className="d-flex flex-column w-100">
-          <Nav.Link as={NavLink} to="/chat" className="fw-normal">
-            Chat
+          <Nav.Link as={NavLink} to="/chat" className="leftbar-tab clickable">
+            <div class="mmi-icon-wr">
+              <div class="z-noti-badge-container">
+                <div class="z-noti-badge --big --counter --noti-enable --anchor --top-right leftbar-unread-badge">
+                  <i class="fa fa-5_Plus_24_Line z-noti-badge__content --big"></i>
+                </div>
+                <i class="fa fa-comment internal-icon"></i>
+              </div>
+            </div>
           </Nav.Link>
-          <Nav.Link as={NavLink} to="/danh-ba" className="fw-normal">
-            Danh ba
+          <Nav.Link as={NavLink} to="/danh-ba" className="leftbar-tab clickable">
+            <div class="mmi-icon-wr">
+              <div class="z-noti-badge-container">
+                <i class="fa fa-address-book internal-icon"></i>
+              </div>
+            </div>
           </Nav.Link>
         </Nav>
+      </Container>
+      <Container className="nav__tabs__bottom">
+        <AccountSetting
+            handleClose={() => setShowModal(false)}
+            content={modalContent}
+        />
       </Container>
     </Navbar>
   );
 };
+
 
 export default Header;
